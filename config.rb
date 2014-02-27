f8755cf993bbde9380fee109e3aeeef4c70234be
Lucy Bain <lucybain@gmail.com>
2014-02-27 12:07:47 +1100

    trying a redirect

--- config.rb
+++ config.rb
@@ -38,23 +38,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+redirect "udacity2", to: "resources/udacity-python-study-group"
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-end
 
 ###
 # Compass
