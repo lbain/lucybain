7876c1147edd2f2da5981dd71340e776688824b9
Lucy Bain <lucybain@gmail.com>
2014-07-13 22:12:22 +1000

    update calculator demo

--- build/code-sydney-work/week-1/calculator/calculator.js
+++ build/code-sydney-work/week-1/calculator/calculator.js
@@ -1,72 +1,90 @@
-var numbers_pressed = [];
-var numbers_actions = [];
+var Math = {
+  add: function(num1, num2) {
+    return num1 + num2;
+  },
 
-function add(num1, num2) {
-  return num1 + num2;
-}
+  subtract: function(num1, num2) {
+    return num1 - num2;
+  },
 
-function subtract(num1, num2) {
-  return num1 - num2;
-}
+  multiply: function(num1, num2) {
+    return num1 * num2;
+  },
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-function clear() {
-  numbers_actions = [];
-  numbers_pressed = [];
-  $('#display').val('0');
-}
-
-function equal(){
-  calculate_current();
-}
-
-function action_to_function(action) {
-  var actions = { '+': add,
-                  '-': subtract,
-                  '*': multiply,
-                  '/': divide
-  };
-  return actions[action];
-}
-
-function special_methods(action) {
-  var actions = { 'cl': clear,
-                  '=': equal
-                };
-  return actions[action]();
-}
-
-function calculate_current(){
-  if (numbers_actions.length > 2) {
-    var method = action_to_function(numbers_actions[1]);
-    var current = method(numbers_actions[0], numbers_actions[2]);
-    numbers_actions = [current];
-    $('#display').val(current);
+  divide: function(num1, num2) {
+    return num1 / num2;
   }
 }
 
-function clear_numbers_pressed() {
-  if (numbers_pressed.length > 0) {
-    numbers_actions.push( parseInt(numbers_pressed.join('')) );
-    numbers_pressed = [];
-  }
-}
+var Calculator = {
 
-function push_action(action) {
-  clear_numbers_pressed();
+  numbers_pressed: [],
+  numbers_actions: [],
+  display: 0,
 
-  if (action == 'cl' || action == '=') {
-    special_methods(action);
-  } else {
-    calculate_current();
-    numbers_actions.push( action );
+  clear: function() {
+    Calculator.numbers_actions = [];
+    Calculator.numbers_pressed = [];
+    Calculator.display = 0;
+  },
+
+  equal: function(){
+    Calculator.calculate_current();
+  },
+
+  action_to_function: function(action) {
+    var actions = { '+': Math.add,
+                    '-': Math.subtract,
+                    '*': Math.multiply,
+                    '/': Math.divide
+    };
+    return actions[action];
+  },
+
+  special_methods: function(action) {
+    var actions = { 'cl': Calculator.clear,
+                    '=': Calculator.equal
+                  };
+    return actions[action]();
+  },
+
+  calculate_current: function(){
+    if (Calculator.numbers_actions.length > 2) {
+      var action = Calculator.numbers_actions[1];
+      var num1 = Calculator.numbers_actions[0];
+      var num2 = Calculator.numbers_actions[2];
+
+      var method = Calculator.action_to_function(action);
+      var current = method(num1, num2);
+      Calculator.numbers_actions = [current];
+      Calculator.display = current;
+    }
+  },
+
+  clear_numbers_pressed: function() {
+    if (Calculator.numbers_pressed.length > 0) {
+      var full_number = parseInt(Calculator.numbers_pressed.join(''));
+      Calculator.numbers_actions.push( full_number );
+      Calculator.numbers_pressed = [];
+    }
+  },
+
+  push_action: function(action) {
+    Calculator.clear_numbers_pressed();
+
+    if (action == 'cl' || action == '=') {
+      Calculator.special_methods(action);
+    } else {
+      Calculator.calculate_current();
+      Calculator.numbers_actions.push( action );
+    }
+    return Calculator.display;
+  },
+
+  push_number: function(number) {
+    Calculator.numbers_pressed.push(number);
+    Calculator.display = Calculator.numbers_pressed.join('');
+    return Calculator.display;
   }
 }
 
@@ -74,13 +92,12 @@ $( document ).ready(function() {
 
   $( ".number" ).on( "click", function() {
     var number = $( this ).text().trim();
-    numbers_pressed.push(number);
-    $('#display').val(numbers_pressed.join(''));
+    $('#display').val(Calculator.push_number(number));
   });
 
   $( ".action" ).on( "click", function() {
     var action = $( this ).text().trim();
-    push_action( action );
+    $('#display').val(Calculator.push_action( action ));
   });
 
 })
