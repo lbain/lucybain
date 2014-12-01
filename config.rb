77d925db018fdb751849abf2d4130c3ceee7348f
Lucy Bain <lucybain@gmail.com>
2014-12-01 21:10:42 +1100

    move blog posts into /2014 - getting ready for 2015!

--- config.rb
+++ config.rb
@@ -12,6 +12,7 @@ activate :blog do |blog|
   blog.permalink = "{year}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "{year}/{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "blog/layout"
   # blog.summary_separator = /(READMORE)/
